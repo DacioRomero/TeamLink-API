79c9eef5a8adc8136410d47ec50658224aeefd7d
Dacio Romero <dacioromero@gmail.com>
2018-10-11 21:17:10 -0700

    Add full comments API support, full test coverage

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,15 +6,6 @@ module.exports = app => {
         res.render('comments-new', { playerId: req.params.playerId });
     });
 
-    // SHOW Comment
-    app.get('/players/:playerId/comments/:id', (req, res) => {
-        Comment.findById(req.params.id)
-        .then(comment => {
-            res.render('comments-show', { comment: comment });
-        })
-        .catch(console.error);
-    });
-
     // EDIT Comment
     app.get('/players/:playerId/comments/:id/edit', (req, res) => {
         Comment.findById(req.params.id)
@@ -25,6 +16,18 @@ module.exports = app => {
     });
 
     // INDEX Comment
+    // API
+    app.get('/api/players/:playerId/comments', (req, res) => {
+        Comment.find({ playerId: req.params.playerId })
+        .then(comments => {
+            res.status(200).send(comments);
+        }).catch((err) => {
+            console.error(err);
+            res.status(400).send(err);
+        });;
+    });
+
+    // Regular
     app.get('/players/:playerId/comments', (req, res) => {
         let playerId = req.params.playerId
         Comment.find({ playerId: playerId })
@@ -34,9 +37,10 @@ module.exports = app => {
         .catch(console.error);
     });
 
+
     // CREATE Comment
+    // API
     app.post('/api/players/:playerId/comments', (req, res) => {
-        console.log(req.body)
         Comment.create(req.body)
         .then(comment => {
             res.status(200).send(comment);
@@ -46,35 +50,40 @@ module.exports = app => {
         });;
     });
 
+    // Regular
     app.post('/players/:playerId/comments', (req, res) => {
         Comment.create(req.body)
-        .then(() => {
-            res.redirect('.');
+        .then(comment => {
+            res.redirect(`/players/${req.params.playerId}/comments/${comment._id}`);
         })
         .catch(console.error);
     });
 
-    // DESTROY Comment
-    app.delete('/api/players/:playerId/comments/:id', (req, res) => {
-        let id = req.params.id
-        Comment.findByIdAndRemove(id)
+
+    // SHOW Comment
+    // API
+    app.get('/api/players/:playerId/comments/:id', (req, res) => {
+        Comment.findById(req.params.id)
         .then(comment => {
             res.status(200).send(comment);
         }).catch((err) => {
             console.error(err);
             res.status(400).send(err);
-        });
+        });;
     });
 
-    app.delete('/players/:playerId/comments/:id', (req, res) => {
-        Comment.findByIdAndRemove(req.params.id)
-        .then(() => {
-            res.redirect('.');
+    // Regular
+    app.get('/players/:playerId/comments/:id', (req, res) => {
+        Comment.findById(req.params.id)
+        .then(comment => {
+            res.render('comments-show', { comment: comment });
         })
         .catch(console.error);
     });
 
+
     // UPDATE Comment
+    // API
     app.put('/api/players/:playerId/comments/:id', (req, res) => {
         Comment.findByIdAndUpdate(req.params.id, req.body)
         .then(comment => {
@@ -90,10 +99,34 @@ module.exports = app => {
         });
     });
 
+    // Regular
     app.put('/players/:playerId/comments/:id', (req, res) => {
         Comment.findByIdAndUpdate(req.params.id, req.body)
         .then(() => {
-            res.redirect('.');
+            res.redirect(`/players/${req.params.playerId}/comments/${req.params.id}`);
+        })
+        .catch(console.error);
+    });
+
+
+    // DESTROY Comment
+    // API
+    app.delete('/api/players/:playerId/comments/:id', (req, res) => {
+        let id = req.params.id
+        Comment.findByIdAndRemove(id)
+        .then(comment => {
+            res.status(200).send(comment);
+        }).catch((err) => {
+            console.error(err);
+            res.status(400).send(err);
+        });
+    });
+
+    // Regular
+    app.delete('/players/:playerId/comments/:id', (req, res) => {
+        Comment.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.redirect(`/players/${req.params.playerId}/comments`);
         })
         .catch(console.error);
     });
