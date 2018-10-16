56e77f68315d0d9ea227418cf33a0f7d09eedeb0
Dacio Romero <dacioromero@gmail.com>
2018-10-16 14:33:29 -0700

    Fix and update client main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,25 +2,29 @@
 var comments = document.getElementById("comments")
 var commentModal = document.getElementById("commentModal")
 var commentForm = commentModal.querySelector("form");
+
 var playerId = commentModal.getAttribute('data-player-id');
+
 commentForm.reset();
 
 comments.addEventListener('click', function (e) {
     var deleteButton = e.target.closest('.deleteComment') || e.target;
+
     if (deleteButton.classList.contains('deleteComment')) {
         var commentId = deleteButton.getAttribute("data-comment-id")
 
         axios.delete(`/api/players/${playerId}/comments/${commentId}`)
-        .then(response => {
-            console.log(document.getElementById(commentId).parentElement)
+        .then(function (response) {
             document.getElementById(commentId).parentElement.remove();
         })
         .catch(console.error);
     }
 }, false);
 
-document.getElementById("newComment").addEventListener("click", function (e) {
-    var formData = new FormData(commentForm);
+commentForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    var formData = new FormData(e.target);
     var commentObject = {};
     formData.forEach(function (value, key) {
         commentObject[key] = value;
@@ -28,14 +32,14 @@ document.getElementById("newComment").addEventListener("click", function (e) {
 
     axios.post(`/api/players/${playerId}/comments`, commentObject)
     .then(function (response) {
-        console.log(response.data);
         comment = response.data;
+        // TODO: Replace with server side rendering?
         comments.innerHTML =
         `<div class="col-4">
             <a class="btn btn-warning deleteComment" data-comment-id="${comment._id}"><i class="fas fa-trash"></i></a>
             <div class="card" id="${comment._id}">
                 <div class="card-body px-2">
-                    <a href="comments/${comment._id}">
+                    <a href="/players/${comment.playerId}/comments/${comment._id}">
                         <h5 class="card-title">${comment.battletag}</h5>
                     </a>
                     <p class="card-text">${comment.content}</p>
@@ -44,8 +48,8 @@ document.getElementById("newComment").addEventListener("click", function (e) {
         </div>
         ${comments.innerHTML}`;
 
-        $("#commentModal").modal('hide'); // jQuery required to close modal
-        commentForm.reset()
+        $(commentModal).modal('hide'); // jQuery required to close modal
+        e.target.reset()
     })
     .catch(console.error);
 });
