e58b54685924aa16c01091b41c5fb352cf9b4f76
Dacio Romero <dacioromero@gmail.com>
2018-10-17 10:12:23 -0700

    Fix index route, improve Player show

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home', { teams: values[0], players: values[1] });
+    res.render('home');
 });
 
 module.exports = router
